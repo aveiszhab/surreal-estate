3ed27470931e094dce9b60e3909d1921562b0023
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-07-04 11:46:59 +0100

    added font family, Airbnb styling and prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
-     <h2>Surreal Estate</h2>
+      <h2>Surreal Estate</h2>
     </div>
   );
 }
