6ca6aa3330b11d25e52930e15c3eb937413202ad
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-09-17 10:32:21 +0100

    Updates

---
+++ precache-manifest.b1fc4b48398face2c2018761f281967f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eeccc3cd87f8e268f34b834cfeb8a74",
+    "url": "/surreal-estate/index.html"
+  },
+  {
+    "revision": "8204b825e525cdeff89c",
+    "url": "/surreal-estate/static/css/main.3f488e17.chunk.css"
+  },
+  {
+    "revision": "18507f655d0b683b6e77",
+    "url": "/surreal-estate/static/js/2.a2a8ff40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/surreal-estate/static/js/2.a2a8ff40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8204b825e525cdeff89c",
+    "url": "/surreal-estate/static/js/main.17778923.chunk.js"
+  },
+  {
+    "revision": "979a0f011893effc2b15",
+    "url": "/surreal-estate/static/js/runtime-main.5d23722d.js"
+  }
+]);
